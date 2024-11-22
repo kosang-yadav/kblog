abd6de94447cec7c98e85dd1f040ea8e2feb7af2
KOSANG YADAV <kosangyadav2004@gmail.com>
2024-11-23 00:48:55 +0530

    gpt copy paste error

--- src/components/YinYang/YinYang.jsx
+++ src/components/YinYang/YinYang.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import styled,{keyframes} from "styled-components";
-import { Yinyang } from "./svgs";
+import { Yinyang } from "./Svgs";
 import { useNavigate } from "react-router-dom";
 
 const clickAnimation = keyframes`
